7942026ea7bcb135fdb1dfa1886de72b40ec1232
leizikangMacbookAirlocal <lzkhaoshua@icloud.com>
2024-09-04 10:18:36 +0800

    添加APP版本

--- Sources/ZKCompoments/App/ZKConstant.swift
+++ Sources/ZKCompoments/App/ZKConstant.swift
@@ -23,4 +23,6 @@ public let screenW = Device.current.screenSize.width
 /// 设备屏幕高
 public let screenH = Device.current.screenSize.height
 
+/// app 版本
+public let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
 
