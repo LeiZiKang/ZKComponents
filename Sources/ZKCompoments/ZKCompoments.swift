079f091b0ced5a481ddc7989f8a0cf479ba50401
Kang <3418498315@qq.com>
2024-09-06 08:19:08 +0800

    添加复制到剪切板

--- Sources/ZKCompoments/ZKCompoments.swift
+++ Sources/ZKCompoments/ZKCompoments.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+
+/// 复制到剪切板
+public func copyToClipboard(text: String, completion: @escaping() -> Void) {
+    UIPasteboard.general.string = text
+    completion()
+}
