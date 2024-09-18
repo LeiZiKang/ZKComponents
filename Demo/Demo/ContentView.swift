f8c23566345f9ee372ce85365d1d1f406883d5be
Kang <3418498315@qq.com>
2024-09-18 16:11:47 +0800

    添加播放资源文件

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZKPlayer(url: "爱的迫降", timecodes: [], showTimeCodeList: false)
     }
 }
 
+extension String {
+    
+    /// 从bundle中获取视频资源url
+    func bundleUrl() -> URL {
+        return Bundle.main.url(forResource: self, withExtension: "mp4")!
+    }
+}
 #Preview {
     ContentView()
 }
