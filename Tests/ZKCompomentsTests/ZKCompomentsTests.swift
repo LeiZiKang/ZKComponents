7012e0cbb7f72890562e448b690b13eb59e3fa16
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-10-22 10:56:46 +0800

    uodate readme

--- Tests/ZKCompomentsTests/ZKCompomentsTests.swift
+++ Tests/ZKCompomentsTests/ZKCompomentsTests.swift
@@ -9,6 +9,19 @@ final class ZKCompomentsTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
         let name = .appDisplayName
+        let version = .appVersion
         printf(name)
+        
+        let width = screenW
+        let height = screenH
+        
+        Device.current.screenSize
+        
+        let a = 28.3213123
+        let b: String  = a.twoDecimalPlacesStr()
+        printf(a.twoDecimalPlacesStr())
+        
+        let str = "https://github.com/"
+        let url: URL? = str.onlineUrl()
     }
 }
