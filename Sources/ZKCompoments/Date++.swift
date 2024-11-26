98714ffa4e7765360e4c7bd5e61d2066f61e43ea
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-11-27 03:12:40 +0800

    feat: 添加randomDate 的 fatalError 方法

--- Sources/ZKCompoments/Date++.swift
+++ Sources/ZKCompoments/Date++.swift
@@ -10,8 +10,10 @@ import Foundation
 extension Date {
     
     /// 生成一个随机日期
-    public static func randomDate(start: Date = .distantPast, end: Date = .distantFuture) -> Date? {
-        guard start <= end else { return nil } // 确保起始日期小于等于结束日期
+    public static func randomDate(start: Date = .distantPast, end: Date = .distantFuture) -> Date {
+        guard start <= end else {
+            fatalError("确保起始日期小于等于结束日期")
+        } 
         
         let timeInterval = end.timeIntervalSince(start)
         let randomInterval = TimeInterval.random(in: 0...timeInterval)
